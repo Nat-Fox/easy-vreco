76f8bfd1d660abb396f63bad8a1ac00881f77d03
Natalia Garrido <natgramirez@outlook.com>
2017-06-19 21:51:31 -0400

    agregando ruta y cambiando marker por defecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function initMap() {
-
+    /***************** Inicializando el mapa ********************/
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
         center: { lat: -9.1191427, lng: -77.0349046 },
@@ -8,15 +8,41 @@ function initMap() {
         streetViewControl: false
     });
 
-    // Agregando autocompletado de primer input
-    var input = (document.getElementById('first-input'));
+
+    var icons = {
+        start: new google.maps.MarkerImage(
+            'assets/img/bici.png',
+            new google.maps.Size(50, 80),
+            // The origin point (x,y)
+            new google.maps.Point(0, 0),
+            // The anchor point (x,y)
+            new google.maps.Point(22, 32)
+
+        ),
+        end: new google.maps.MarkerImage(
+            'assets/img/bici.png',
+            new google.maps.Size(50, 50),
+            // The origin point (x,y)
+            new google.maps.Point(0, 0),
+            // The anchor point (x,y)
+            new google.maps.Point(22, 32)
+        )
+    };
+
+    /***************** Agregando autocompletado de primer input ********************/
+    var input = document.getElementById('first-input');
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
 
-    // Agregando autocompletado de segundo input
-    var input = (document.getElementById('second-input'));
+    /***************** Agregando autocompletado del segundo input ********************/
+    var input = document.getElementById('second-input');
     var autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.bindTo('bounds', map);
+
+    /***************** Evento para encontrar la localización actual ********************/
+
+    document.getElementById('encuentrame').addEventListener('click', buscar);
+
 
     function buscar() {
         if (navigator.geolocation) {
@@ -25,24 +51,73 @@ function initMap() {
         }
     }
 
-    document.getElementById('origen').addEventListener('click', buscar);
     var latitud, longitud;
+    // var funcionExito = function(posicion) {
+    //     latitud = posicion.coords.latitude;
+    //     longitud = posicion.coords.longitude;
 
-    var funcionExito = function(posicion) {
-        latitud = posicion.coords.latitude;
-        longitud = posicion.coords.longitude;
+    //     // var miUbicacion = new google.maps.Marker({
+    //     //     position: { lat: latitud, lng: longitud },
+    //     //     animation: google.maps.Animation.DROP,
+    //     //     map: map
+    //     // });
 
-        var miUbicacion = new google.maps.Marker({
-            position: { lat: latitud, lng: longitud },
-            animation: google.maps.Animation.DROP,
-            map: map
-        });
+    //     console.info('Exito yeah');
+    //     var miUbicacion = addMarker({
+    //         position: { lat: latitud, lng: longitud },
+    //         animation: google.maps.Animation.DROP,
+    //         map: map
+    //     });
 
-        map.setZoom(17);
-        map.setCenter({ lat: latitud, lng: longitud });
-    }
+    //     map.setZoom(17);
+    //     map.setCenter({ lat: latitud, lng: longitud });
+    // }
 
     var funcionError = function(error) {
         alert('Tenemos un problema con encontrar tu ubicación');
     }
-}
+
+    /***************** Para trazar la ruta ********************/
+    var directionsService = new google.maps.DirectionsService();
+    // suppressMarkers: true -> suprimir los iconos por defecto
+    var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true });
+
+    document.getElementById('first-input').addEventListener('change', onChangeHandler);
+    document.getElementById('second-input').addEventListener('change', onChangeHandler);
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+            origin: document.getElementById('first-input').value,
+            destination: document.getElementById('second-input').value,
+            travelMode: 'DRIVING'
+        }, function(response, status) {
+            if (status === 'OK') {
+                directionsDisplay.setDirections(response);
+                var leg = response.routes[0].legs[0];
+                makeMarker(leg.start_location, icons.start, '');
+                makeMarker(leg.end_location, icons.end, '');
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+
+    document.getElementById('btn-ruta').addEventListener('click', onChangeHandler);
+
+    /***************** Para cambiar el icono ********************/
+    function makeMarker(position, icon, title) {
+        new google.maps.Marker({
+            position: position,
+            map: map,
+            icon: icon,
+            title: title
+        });
+    }
+
+};
